fa2c4712d1de740fd78d7f04fb93d873cca116ae
haim hod gilboa <haim@newclass.co.il>
2022-04-24 15:00:31 +0300

    Start

---
+++ Tech.MultiTasks.Console/DirectoryListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech.MultiTasks.Console
+{
+    internal class DirectoryListener
+    {
+        readonly string FolderName;
+        public bool IsRunning = true;
+        public DirectoryListener(string folderName)
+        {
+            FolderName = folderName;
+            RunTask();
+        }
+        public int NumberOfFiles = 0;
+        public Task RunTask()
+        {
+            Task task = Task.Factory.StartNew(() => { 
+                
+                while (IsRunning)
+                {
+                    
+                    NumberOfFiles = System.IO.Directory.GetFiles(FolderName).Length;
+                    System.Threading.Thread.Sleep(4000);
+                    // run paralely
+                }
+
+
+            });
+
+            return task;
+        }
+    }
+}
--- Tech.MultiTasks.Console/Program.cs
+++ Tech.MultiTasks.Console/Program.cs
@@ -10,6 +10,26 @@ namespace Tech.MultiTasks.Console
     {
         static void Main(string[] args)
         {
+            int ite = 100;
+            int currentFileCount = 0;
+            DirectoryListener listener = new DirectoryListener(@"C:\Code\CSV\AccidentsReport\AccidentsUI\Data\");
+            while(listener.IsRunning)
+            {
+                if(currentFileCount!= listener.NumberOfFiles)
+                {
+                   
+                   // Console.WriteLine($"Files count has been changed to {listener.NumberOfFiles}");
+                    currentFileCount = listener.NumberOfFiles;
+                }
+                System.Threading.Thread.Sleep(5000);
+                ite--;
+                if(ite==0)
+                {
+                    listener.IsRunning = false;
+                }
+            }
+
+           // listener.IsRunning = false;
         }
     }
 }
