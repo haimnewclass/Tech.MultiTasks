e73901ff16e0bf44a66879232f3f81abad11a5a3
haim hod gilboa <haim@newclass.co.il>
2022-04-27 11:25:28 +0300

    LINQ

--- Tech.MultiTasks.Console/Program.cs
+++ Tech.MultiTasks.Console/Program.cs
@@ -8,8 +8,39 @@ namespace Tech.MultiTasks.Console
 {
     internal class Program
     {
+        class Ball
+        {
+            public int size;
+            public string name;
+        }
         static void Main(string[] args)
         {
+
+            int[] arr = { 234, 2345, 34, 1, 2, 4, 3, 2, 3, 4, 3, 4, 3, 23, 2, 342, 34234, 3 };
+
+            var q = from num in arr
+                    where num > 5 && num < 800
+                    orderby num
+                    select num;
+
+            var q1 = from num in arr
+                     where num > 5 && num < 800
+                     orderby num
+                     select num*num + 10;
+
+
+            List<Ball> list = new List<Ball>();
+            list.Add(new Ball() { name = "Fifa", size = 5 });
+            list.Add(new Ball() { name = "Fiba", size = 7 });
+            list.Add(new Ball() { name = "Fifa", size = 7 });
+
+            var q3 = from i in list
+                     where i.size == 7
+                     select i;
+
+
+            var list3 =  q3.ToList<Ball>();
+            
             int ite = 100;
             int currentFileCount = 0;
 
